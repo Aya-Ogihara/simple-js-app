06fc08c0df5fe8d4e5504683b09d1b863f626988
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-09-18 15:55:06 +1200

    Add a new variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
 
+
+let pokemonList = [{
+  name: 'Bulbasaur',
+  nameInJapanese: 'フシギダネ',
+  knownAs: 'Seed Pokémon',
+  height: 0.7,
+  type: ['grass', 'poison']
+}, {
+  name: 'Charmander',
+  nameInJapanese: 'ヒトカゲ',
+  knownAs: 'Lizard Pokémon',
+  height: 0.6,
+  type: 'fire'
+}, {
+  name: 'Squirtle',
+  nameInJapanese: 'ゼニガメ',
+  knownAs: 'Young Turtle Pokémon',
+  height: 0.5,
+  type: 'water'
+}, {
+  name: 'Pikachu',
+  nameInJapanese: 'ピカチュウ',
+  knownAs: 'Mouse Pokémon',
+  height: 0.4,
+  type: 'electric'
+}];
+
+
+
+
+/* Task 1-1
 alert('Hello world');
 
 let favoriteFood = 'chocolate';
 document.write(favoriteFood);
+*/
